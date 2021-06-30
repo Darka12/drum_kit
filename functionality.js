974d29bdba8502b2f56ab65e8ed19aeb3f059120
Oliver Horwood <thereviloproject@gmail.com>
2021-06-30 23:11:18 +0100

    added currenttime so that the audio is started from the beginning on click. Added comments also

--- functionality.js
+++ functionality.js
@@ -1,6 +1,7 @@
 // Listens for the keydown event and when keydown event happens run function
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    if(!audio) return;
-    audio.play();
+window.addEventListener('keydown', function(e) { 
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`); // Checks if the key pressed has an audio element 
+    if(!audio) return; // If the key does not have an audio element then stop the function
+    audio.currentTime = 0; // Rewinds the audio to the beginning on every click
+    audio.play(); // Plays the relevent audio
 })
