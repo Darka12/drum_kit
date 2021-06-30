8cb2ae8b92b1bdc0f10891cd850bc3efdbbf57a9
Oliver Horwood <thereviloproject@gmail.com>
2021-06-30 22:37:41 +0100

    Added basic audio play function and replaced the audio links in the html file with working ones

--- functionality.js
+++ functionality.js
@@ -1,4 +1,6 @@
 // Listens for the keydown event and when keydown event happens run function
 window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    if(!audio) return;
+    audio.play();
 })
