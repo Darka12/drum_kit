31bba402812fcc1b276bd33620f67e02c5c3bea9
Oliver Horwood <thereviloproject@gmail.com>
2021-07-01 07:07:20 +0100

    styled button words, and updated comments in the js file

--- functionality.js
+++ functionality.js
@@ -1,6 +1,6 @@
 // Listens for the keydown event and when keydown event happens run function
 window.addEventListener('keydown', function(e) { 
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`); // Checks if the key pressed has an audio element 
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`); // Checks if the key pressed has a corresponding audio element (checks with key codes)
     if(!audio) return; // If the key does not have an audio element then stop the function
     audio.currentTime = 0; // Rewinds the audio to the beginning on every click
     audio.play(); // Plays the relevent audio
