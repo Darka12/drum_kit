c1ac764e2ce1290d8fbc11de6633f155a3881640
Oliver Horwood <thereviloproject@gmail.com>
2021-07-02 07:32:25 +0100

    added function to remove css transition after 250ms

--- functionality.js
+++ functionality.js
@@ -3,8 +3,12 @@ window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`); // Checks if the key pressed has a corresponding audio element (checks with key codes)
     const transition = document.querySelector(`div[data-key = "${e.keyCode}"]`);
     if(!audio) return; // If the key does not have an audio element then stop the function
-    transition.classList.add('playing');
+    transition.classList.add('playing'); // Adds css .playing transition when a key with a corresponding div element exists
     audio.currentTime = 0; // Rewinds the audio to the beginning on every click
     audio.play(); // Plays the relevent audio  
+
+    // Removes the css .playing transition after 250ms
+    setTimeout(function() {
+        transition.classList.remove('playing'); }, 250);
 })
 
